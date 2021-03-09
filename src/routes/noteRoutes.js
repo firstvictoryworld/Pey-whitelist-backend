266f2bd8ec878c29f2f6b2e44374150ba1f3dd5b
admin <admin@admin.com>
2021-03-09 12:42:04 +0100

    done

---
+++ src/routes/noteRoutes.js
@@ -0,0 +1,21 @@
+const notesController = require('../controllers/notesController');
+
+module.exports = (app) => {
+  //# create a note
+  app.post('/api/whitelists', notesController.create);
+
+  //#get the list of notes
+  app.get('/api/whitelists', notesController.fetch);
+
+  //#get a single note
+  app.get('/api/whitelists/:id', notesController.get);
+
+  //#check if this address is belong to whitelist or not
+  app.get('/whitelists/:id', notesController.check);
+
+  //#update a note
+  app.put('/api/whitelists/:id', notesController.update);
+
+  //#delete a note
+  app.delete('/api/whitelists/:id', notesController.delete);
+};
